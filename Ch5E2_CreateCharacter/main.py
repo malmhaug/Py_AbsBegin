457edb9c7f965208a01985d76c612639b1459295
Jim Malmhaug <malmhaug@gmail.com>
2015-12-30 11:37:11 +0100

    Update main.py in Ch5E2

--- Ch5E2_CreateCharacter/main.py
+++ Ch5E2_CreateCharacter/main.py
@@ -1,9 +1,8 @@
 # Project: Ch5E2_CreateCharacter
 # Author: Jim-Kristian Malmhaug
-# Date: 19 Des 2015
+# Date: 29 Des 2015
 
 # About: This program lets the user build skills for a character in a RPG
-
 print("""
 	
 	            ***The RPG character building program***
@@ -12,15 +11,15 @@ print("""
 	               different skills set for your hero
 	            
 	""")
-
+	
 pool = 30  # pool of points
-skill_1 = 0  # skill set 1
-skill_2 = 0  # skill set 2
-skill_3 = 0  # skill set 3
-skill_4 = 0  # skill set 4
+strength = 0  # skill set strength
+health = 0  # skill set health
+wisdom = 0  # skill set wisdom
+dexterity = 0  # skill set dexterity
+choice = None  # the menu choice
 
 while choice != 0:
-   
    print("""
 	
 	         Choose an option:
@@ -32,34 +31,111 @@ while choice != 0:
 	
 	   """)
 	   
-   choice = input("Option: ")
-   
+   choice = int(input("Option: "))
    if choice == 0:
-      break
-   elsif choice == 1:
+      print("Exited")
+   elif choice == 1:
       print("""
       	
       	Skills:
       	
-      	      skill 1 is {0}
-      	      skill 2 is {1}
-      	      skill 3 is {2}
-      	      skill 4 is {3}
+      	      Strength is {0}
+      	      Health is {1}
+      	      Wisdom is {2}
+      	      Dexterity is {3}
+      	      
+      	      Skill pool is {4}
       	
-      	""".format(skill_1, skill_2, skill_3, skill_4))
-   elsif choice == 2:
+      	""".format(strength, health, wisdom, dexterity, pool))
+      	
+   elif choice == 2:
       print("""
       	
       	Choose skill to reduce:
       	      
       	      0. Exit
-      	      1. skill 1
-      	      2. skill 2
-      	      3. skill 3
-      	      4. skill 4
+      	      1. Strength
+      	      2. Health
+      	      3. Wisdom
+      	      4. Dexterity
       	   
-      	""")
+      	""")	
+      reduce_choice = int(input("Choice: "))
+      if reduce_choice is 0:
+          print("Exited")
+      elif reduce_choice is 1 and strength > 0:
+          reduce_value = int(input("Reduce strength to -> "))
+          if reduce_value < strength and reduce_value >= 0:
+             pool = pool + (strength - reduce_value)
+             strength = reduce_value
+          else:
+             print("Not valid entry")
+      elif reduce_choice is 2 and health > 0:
+          reduce_value = int(input("Reduce health to -> "))
+          if reduce_value < health and reduce_value >= 0:
+             pool = pool + (health - reduce_value)
+             health = reduce_value
+          else:
+             print("Not valid entry")
+      elif reduce_choice is 3 and wisdom > 0:
+          reduce_value = int(input("Reduce wisdom to -> "))
+          if reduce_value < wisdom and reduce_value >= 0:
+             pool = pool + (wisdom - reduce_value)
+             wisdom = reduce_value
+          else:
+             print("Not valid entry")
+      elif reduce_choice is 4 and dexterity > 0:
+          reduce_value = int(input("Reduce dexterity to -> "))
+          if reduce_value < dexterity and reduce_value >= 0:
+             pool = pool + (dexterity - reduce_value)
+             dexterity = reduce_value
+          else:
+             print("Not valid entry")
+      else:
+          print("Not valid entry")
+   
+   elif choice == 3 and pool > 0:
+      print("""
       	
-      reduce_choice = input("Choice: ")
-      # Code here!
-      
+      	Choose skill to add to:
+      	      
+      	      0. Exit
+      	      1. Strength
+      	      2. Health
+       	      3. Wisdom
+      	      4. Dexterity
+      	   
+      	""")	
+      add_choice = int(input("Choice: "))
+      if add_choice is 0:
+          print("Exited")
+      elif add_choice is 1 and strength < 30:
+          add_value = int(input("Put strength to -> "))
+          if add_value > strength and add_value <= 30:
+             pool = pool - (add_value - strength)
+             strength = add_value
+          else:
+             print("Not valid entry")
+      elif add_choice is 2 and health < 30:
+          add_value = int(input("Put health to -> "))
+          if add_value > health and add_value <= 30:
+             pool = pool - (add_value - health)
+             health = add_value
+          else:
+             print("Not valid entry")
+      elif add_choice is 3 and wisdom < 30:
+          add_value = int(input("Put wisdom to -> "))
+          if add_value > wisdom and add_value <= 30:
+             pool = pool - (add_value - wisdom)
+             wisdom = add_value
+          else:
+             print("Not valid entry")
+      elif add_choice is 4 and dexterity < 30:
+          add_value = int(input("Put dexterity to -> "))
+          if add_value > dexterity and add_value <= 30:
+             pool = pool - (add_value - dexterity)
+             dexterity = add_value
+          else:
+             print("Not valid entry")
+      else:
+          print("Not valid entry")
