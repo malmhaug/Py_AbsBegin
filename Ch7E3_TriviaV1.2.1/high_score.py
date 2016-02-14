b88b995c659e5da5dbba24dd3f25b162df4ca74c
Jim Malmhaug <malmhaug@gmail.com>
2016-02-14 22:27:21 +0100

    Improved Ch7E3

--- Ch7E3_TriviaV1.2.1/high_score.py
+++ Ch7E3_TriviaV1.2.1/high_score.py
@@ -12,27 +12,50 @@ def open_file(file_name, mode):
         return the_file
 
 def high_scores(score):
+    new_lines = []
+    no_name = 0
+
     name = input("\nEnter your name --> ")
 
     score_file = open_file("high_scores.txt", "r")
     lines = score_file.readlines()
     score_file.close()
 
-    score_file = open_file("high_scores.txt", "a")
-
+    # If file is empty, write name and score
     if len(lines) < 1:
-        score_file.write(name + ':' + str(score) + '\n')
+        print("Empty file")
+        new_lines.append(str(score) + ':' + name + '\n')
 
+    # Transfer from lines to new_lines
     for entry in lines:
-        if name.lower() in entry:
-            score_file.write(name + ':' + str(score) + '\n')
-            break
-        if name.lower() not in entry:
-            score_file.write(name + ':' + str(score) + '\n')
-            break
+        new_lines.append(entry)
 
+    # Check for new names
+    for entry in range(len(new_lines)):
+        if new_lines[entry].find(name) == -1:
+            no_name += 1
+    if no_name >= len(new_lines):
+        print("New name")
+        new_lines.append(str(score) + ':' + name + '\n')
+
+    # Check score for names already in list
+    for entry in range(len(new_lines)):
+        position = new_lines[entry].find(':')
+        old_score = int(new_lines[entry][:position])
+        if name.lower() in new_lines[entry] and old_score < score:
+            print("New score for person in list")
+            new_lines[entry] = str(score) + ':' + name + '\n'
+
+    # Sort list
+    new_lines.sort(reverse=True)
+
+    print("HIGH SCORE\n")
+    for entry in new_lines:
+        print(entry)
+
+    score_file = open_file("high_scores.txt", "w")
+    score_file.writelines(new_lines)
     score_file.close()
 
-
 if __name__ == '__main__':
-    high_scores(14)
+    high_scores(16)
